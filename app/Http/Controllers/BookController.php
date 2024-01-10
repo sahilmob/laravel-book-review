41dc28232982905a6bebfa821c3e8fc9cf0b14c6
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2024-01-10 22:47:50 +0300

    create books index

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,15 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BookController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $title = $request->input('title');
+
+        $books = Book::when($title, function($query, $title){
+            return $query->title($title);
+        })->get();
+
+        return view('books.index', [
+            'books' => $books
+        
+        ]);
     }
 
     /**
