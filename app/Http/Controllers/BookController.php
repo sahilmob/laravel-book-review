9bbcd0b964bec16c7655b52e2dd8f85828ca96d6
Sahil H. Moubaideen <sahil.mubaideen@network.global>
2024-01-11 11:08:39 +0300

    cache reviews and invalidate cache

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -60,11 +60,14 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
+        $cacheKey = 'book:' . $book->id;
+        $book = cache()->remember($cacheKey, 3600, fn() => $book->load([
+            'reviews' => fn($query) => $query->latest()
+        ]));
+
 
         return view('books.show', [
-            'book' => $book->load([
-                'reviews' => fn($query) => $query->latest()
-            ])
+            'book' => $book
         ]);
     }
 
